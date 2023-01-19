0e2e5687a9f446755d0b4db4aa88e0f4a7ca3247
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2023-01-19 20:27:16 +0800

    Update ins-xray.sh

--- install/ins-xray.sh
+++ install/ins-xray.sh
@@ -1,5 +1,5 @@
 # // wget https://github.com/${GitUser}/
-GitUser="huaweipadu"
+GitUser="zahwanugrah"
 
 # // MY IPVPS
 export MYIP=$(curl -sS ipv4.icanhazip.com)
@@ -7,7 +7,7 @@ export MYIP=$(curl -sS ipv4.icanhazip.com)
 # // GETTING
 VALIDITY () {
     today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/lol/main/ipvps.conf | grep $MYIP | awk '{print $4}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
     else
@@ -16,7 +16,7 @@ VALIDITY () {
     exit 0
 fi
 }
-IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/lol/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 VALIDITY
